cbd7536c099b2fd4a03c81897871e55a81b71620
davisws <davisepic2241@gmail.com>
2025-07-16 14:52:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -94,6 +94,37 @@ io.use(sharedSession(session({
   saveUninitialized: false,
 })));
 
+const tawkToScript = `
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/68780da23606072bf84a2d97/1j0afagrk';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+`;
+
+// Middleware to serve index.html with Tawk.to injected before </body>
+app.get('/', (req, res) => {
+  const indexPath = path.join(__dirname, 'public', 'index.html');
+  fs.readFile(indexPath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error loading page');
+      return;
+    }
+    // Inject Tawk.to script before </body>
+    const modifiedHtml = data.replace('</body>', `${tawkToScript}\n</body>`);
+    res.send(modifiedHtml);
+  });
+});
+
+
 // Start server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
